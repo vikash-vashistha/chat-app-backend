3c81e9c211a2f8c0bd20c20cda58cd8ff37a8de8
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2023-02-14 22:45:50 +0530

    dk

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://vikashchatapp.netlify.app/",
+    origin: "http://localhost:3000",
     // credentials: true,
   },
 });
