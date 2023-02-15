d69b5e5cc1e14fb85a31cc05079186a8a81dd310
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2023-02-15 12:12:54 +0530

    sl

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const path = require("path");
 
 
 const app = express();
@@ -27,23 +26,6 @@ app.use(notFound);
 app.use(errorHandler);
 
 
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
 
 const PORT = process.env.PORT || 8000;
 
